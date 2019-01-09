bac3824c35729477d4d0c1e66da9c16c3a191a2f
Félix Saparelli <felix@passcod.name>
2019-01-09 20:48:30 +1300

    [constraints] Implement belong ip and subnet constraints

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
 #![forbid(unsafe_code)]
 #![deny(clippy::pedantic)]
 #![allow(clippy::stutter)]
+#![allow(clippy::non_ascii_literal)]
 
-extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 #[cfg_attr(test, macro_use)]
 extern crate serde_json;
 
-extern crate systemstat;
-
 use crate::proto::*;
+use ipnet::IpNet;
 use std::io::Result as IoResult;
 use systemstat::Platform;
 
@@ -37,25 +36,57 @@ impl System {
             .map(|mem| (mem.free.as_usize() as f32) / (mem.total.as_usize() as f32))
     }
 
-    pub fn check_resource(&self, res: &Resource) -> IoResult<bool> {
-        match res {
-            Resource::Memory(MemoryReq::Absolute(free)) => Ok(self.available_memory()? >= *free),
-            Resource::Memory(MemoryReq::Percentage(available)) => {
-                Ok(&self.available_memory_percent()? >= available)
+    #[allow(clippy::cast_precision_loss)]
+    pub fn available_load(&self) -> IoResult<f32> {
+        self.platform
+            .load_average()
+            .map(|load| 100_f32 - (load.one / num_cpus::get() as f32) * 100_f32)
+    }
+
+    #[allow(clippy::expect_fun_call)]
+    pub fn belonging_ips(&self) -> IoResult<Vec<IpNet>> {
+        let mut nets = Vec::new();
+        for net in self.platform.networks()?.values() {
+            for addr in &net.addrs {
+                nets.push(
+                    match addr.addr {
+                        systemstat::data::IpAddr::V4(ip) => format!("{:?}/32", ip),
+                        systemstat::data::IpAddr::V6(ip) => format!("{:?}/128", ip),
+                        _ => continue,
+                    }.parse()
+                        .expect(&format!("Malformed IP address from system {:?}", addr)),
+                );
             }
-            _ => Ok(false),
         }
+        Ok(nets)
+    }
+
+    pub fn check_resource(&self, res: &Resource) -> IoResult<bool> {
+        Ok(match res {
+            Resource::Memory(MemoryReq::Absolute(free)) => self.available_memory()? >= *free,
+            Resource::Memory(MemoryReq::Percentage(available)) => {
+                &self.available_memory_percent()? >= available
+            }
+            Resource::Cpu(CpuReq::Load(available)) => &self.available_load()? >= available,
+            Resource::NetworkBelong(NetReq::IP(ip)) => self.belonging_ips()?.contains(ip),
+            Resource::NetworkBelong(NetReq::Subnet(sub)) => {
+                self.belonging_ips()?.iter().any(|ip| sub.contains(ip))
+            }
+            _ => false,
+        })
     }
 }
 
 fn main() {
-    println!("Hello, world!");
+    println!("🌈 Hello, wonderful world!\n");
 
     let sys = System::new();
     println!(
         "Memory available: {:?} KiB",
         sys.available_memory().unwrap()
     );
+    println!("IPs: {:?}", sys.belonging_ips().unwrap());
+    println!("Inverse load: {:?}", sys.available_load().unwrap());
 
     let memcon1 =
         proto::Constraint::required(proto::Resource::Memory(proto::MemoryReq::Absolute(10_240)));
@@ -72,5 +103,55 @@ fn main() {
         "\n{:?}\nPasses: {}",
         memcon2,
         sys.check_resource(&memcon2.resource).unwrap()
+    );
+
+    let cpucon1 = proto::Constraint::required(proto::Resource::Cpu(proto::CpuReq::Load(10_f32)));
+    println!(
+        "\n{:?}\nPasses: {}",
+        cpucon1,
+        sys.check_resource(&cpucon1.resource).unwrap()
+    );
+
+    let cpucon2 = proto::Constraint::required(proto::Resource::Cpu(proto::CpuReq::Load(89_f32)));
+    println!(
+        "\n{:?}\nPasses: {}",
+        cpucon2,
+        sys.check_resource(&cpucon2.resource).unwrap()
+    );
+
+    let ipcon1 = proto::Constraint::required(proto::Resource::NetworkBelong(proto::NetReq::IP(
+        "::1/128".parse().unwrap(),
+    )));
+    println!(
+        "\n{:?}\nPasses: {}",
+        ipcon1,
+        sys.check_resource(&ipcon1.resource).unwrap()
+    );
+
+    let ipcon2 = proto::Constraint::required(proto::Resource::NetworkBelong(proto::NetReq::IP(
+        "2038::1/128".parse().unwrap(),
+    )));
+    println!(
+        "\n{:?}\nPasses: {}",
+        ipcon2,
+        sys.check_resource(&ipcon2.resource).unwrap()
+    );
+
+    let subcon1 = proto::Constraint::required(proto::Resource::NetworkBelong(
+        proto::NetReq::Subnet("10.0.100.0/24".parse().unwrap()),
+    ));
+    println!(
+        "\n{:?}\nPasses: {}",
+        subcon1,
+        sys.check_resource(&subcon1.resource).unwrap()
+    );
+
+    let subcon2 = proto::Constraint::required(proto::Resource::NetworkBelong(
+        proto::NetReq::Subnet("192.0.2.0/24".parse().unwrap()),
+    ));
+    println!(
+        "\n{:?}\nPasses: {}",
+        subcon2,
+        sys.check_resource(&subcon2.resource).unwrap()
     );
 }
