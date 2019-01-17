a1deb7ee3b36d5ca4e2a00dddbf0617d14558223
Félix Saparelli <felix@passcod.name>
2019-01-18 01:55:04 +1300

    [worker] Stop being generic over Agent

--- src/rpc.rs
+++ src/rpc.rs
@@ -1,8 +1,6 @@
 // Why JSON RPC? Simple, lightweight, well-established, can be hand-written in a pinch
 // Why Websocket? Duplex, inspectable, trivial to secure, can be used from browsers as-is
 
-pub use self::definer::*;
 pub use self::handler::*;
 
-mod definer;
 mod handler;
--- src/server/for_worker.rs
+++ src/server/for_worker.rs
@@ -1,7 +1,8 @@
+use crate::agent::AgentHandle;
 use crate::inflight::Inflight;
 use crate::proto::Worker;
-use crate::rpc::{parse_params, RpcDefiner, RpcHandler};
-use jsonrpc_core::{IoHandler, Params, Value};
+use crate::rpc::RpcHandler;
+use jsonrpc_core::{IoHandler, Params, Result as RpcResult, Value};
 use log::info;
 use serde_json::json;
 
@@ -11,12 +12,12 @@ pub trait WorkerSource {
     fn unregister_worker(&self, name: &str);
 }
 
-pub struct WorkerServer<W: WorkerSource + Clone> {
+pub struct WorkerServer {
     /// Own websocket end
     sender: ws::Sender,
 
     /// Source of worker data
-    source: W,
+    source: AgentHandle,
 
     /// Requests currently awaiting response
     inflight: Inflight,
@@ -25,28 +26,18 @@ pub struct WorkerServer<W: WorkerSource + Clone> {
     rpc: IoHandler,
 }
 
-impl<W: WorkerSource + Clone> WorkerServer<W> {
-    pub fn create(sender: ws::Sender, source: W) -> Self {
-        let mut rpc = IoHandler::new();
-
-        rpc.add_method("worker.register", |params| {
-            let worker: Worker = parse_params(params)?;
-            source.register_worker(worker);
-            Ok(Value::Bool(true))
-        });
-        rpc.add_method("worker.get", |_| Ok(Value::Bool(true)));
-        rpc.add_method("worker.unregister", |_| Ok(Value::Bool(true)));
-
+impl WorkerServer {
+    pub fn create(sender: ws::Sender, source: AgentHandle) -> Self {
         Self {
             sender,
             source,
             inflight: Inflight::default(),
-            rpc,
+            rpc: IoHandler::new(),
         }
     }
 }
 
-impl<W: WorkerSource + Clone> RpcHandler for WorkerServer<W> {
+impl RpcHandler for WorkerServer {
     const PROTOCOL: &'static str = "armstrong/worker";
 
     fn sender(&self) -> &ws::Sender {
@@ -62,28 +53,14 @@ impl<W: WorkerSource + Clone> RpcHandler for WorkerServer<W> {
     }
 }
 
-impl<W: WorkerSource + Clone> RpcDefiner for WorkerServer<W> {
-    fn rpc(&mut self) -> &mut IoHandler {
-        &mut self.rpc
-    }
-
-    fn init_rpc(&mut self) {
-        self.define_method("worker.register", |worker: Worker| {
-            info!("hello world {:?}", worker);
-            // this.source().register_worker(worker);
-            Ok(json!(true))
-        });
-    }
-}
-
-impl<W: WorkerSource + Clone> ws::Handler for WorkerServer<W> {
+impl ws::Handler for WorkerServer {
     fn on_request(&mut self, req: &ws::Request) -> ws::Result<ws::Response> {
         self.rpc_on_request(req)
     }
 
     fn on_open(&mut self, _shake: ws::Handshake) -> ws::Result<()> {
         info!("connection accepted for worker");
-        self.init_rpc();
+        // delegate here
         self.notify(
             "greetings",
             Params::Map(
