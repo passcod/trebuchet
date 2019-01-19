99b1fb595597496eea369a7fe9e140deeff9553d
Félix Saparelli <felix@passcod.name>
2019-01-19 20:49:03 +1300

    [inflight] Use a clonable Sender rather than an Arc<Mutex>
    The only purpose of doing the Arc<Mutex> dance, or the even more 
    complicated ArcSwapOption dance, as I briefly went with writing this 
    commit, was to make Sender Send+Sync+Clone.
    
    A better solution is to use a channel that is already designed to have 
    these guarantees built-in. As a bonus, crossbeam's implementation is 
    reportedly more performant.

--- src/inflight.rs
+++ src/inflight.rs
@@ -1,30 +1,26 @@
 use arc_swap::ArcSwap;
+use crossbeam_channel::{bounded, Receiver, Sender};
 use jsonrpc_core::{Id, Response};
 use log::trace;
 use rpds::HashTrieMap;
-use std::sync::mpsc::{channel, Receiver, Sender};
-use std::sync::{
-    atomic::{AtomicUsize, Ordering},
-    Arc, Mutex,
-};
+use std::sync::atomic::{AtomicUsize, Ordering};
 
 #[derive(Default)]
 pub struct Inflight {
     counter: AtomicUsize,
-    store: ArcSwap<HashTrieMap<Id, Arc<Mutex<Sender<Response>>>>>,
+    store: ArcSwap<HashTrieMap<Id, Sender<Response>>>,
 }
 
 impl Inflight {
     pub fn launch(&self) -> (Id, Receiver<Response>) {
         trace!("incrementic atomic");
         let id = Id::Num(self.counter.fetch_add(1, Ordering::AcqRel) as u64);
-        let (tx, rx) = channel();
+        let (tx, rx) = bounded(1);
 
-        let atx = Arc::new(Mutex::new(tx));
         self.store.rcu(|inner| {
             let id = id.clone();
             trace!("rcu over inflight log to insert {:?}", id);
-            inner.insert(id, atx.clone())
+            inner.insert(id, tx.clone())
         });
 
         (id, rx)
@@ -38,10 +34,9 @@ impl Inflight {
                 inner.remove(&id)
             })
             .get(&id)
-            .map(|atx| {
-                trace!("moving and unwrapping arc over sender");
-                let mtx = Arc::try_unwrap(atx.clone()).unwrap();
-                mtx.into_inner().unwrap()
+            .map(|tx| {
+                trace!("cloning sender");
+                tx.clone()
             })
     }
 }
