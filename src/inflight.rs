306b958498c6f0d9ec26916e62e15e5a234261a5
Félix Saparelli <felix@passcod.name>
2019-03-27 00:36:15 +1300

    [rpc] Fix and simplify inflight log

--- src/inflight.rs
+++ src/inflight.rs
@@ -1,45 +1,50 @@
-use arc_swap::ArcSwap;
 use crossbeam_channel::{bounded, Receiver, Sender};
 use jsonrpc_core::{Id, Response};
 use log::trace;
-use rpds::HashTrieMap;
+use std::collections::HashMap;
 use std::sync::{
     atomic::{AtomicUsize, Ordering},
-    Arc,
+    Arc, Mutex,
 };
 
 #[derive(Clone, Default)]
 pub struct Inflight {
     counter: Arc<AtomicUsize>,
-    store: ArcSwap<HashTrieMap<Id, Sender<Response>>>,
+
+    // Mutex is appropriate here as we're always doing writes, and never just
+    // reading. So there's no need for any structure that optimises for reads!
+    // Perhaps a hashmap with more granular locking would help, though.
+    store: Arc<Mutex<HashMap<Id, Sender<Response>>>>,
 }
 
 impl Inflight {
     pub fn launch(&self) -> (Id, Receiver<Response>) {
         trace!("incrementic atomic");
         let id = Id::Num(self.counter.fetch_add(1, Ordering::AcqRel) as u64);
-        let (tx, rx) = bounded(1);
+        let (tx, rx) = bounded(0);
 
-        self.store.rcu(|inner| {
-            let id = id.clone();
-            trace!("rcu over inflight log to insert {:?}", id);
-            inner.insert(id, tx.clone())
-        });
+        {
+            trace!("obtain inflight store lock");
+            let mut map = self.store.lock().expect("inflight store poisoned");
+            trace!("insert {:?} into inflight log", id);
+            map.insert(id.clone(), tx);
+            trace!("release inflight store lock");
+        }
 
         (id, rx)
     }
 
     pub fn recall(&self, id: &Id) -> Option<Sender<Response>> {
-        self.store
-            .rcu(|inner| {
-                let id = id.clone();
-                trace!("rcu over inflight log to remove {:?}", id);
-                inner.remove(&id)
+        let otx = {
+            trace!("obtain inflight store lock");
+            let mut map = self.store.lock().expect("inflight store poisoned");
+            trace!("remove {:?} from inflight log", id);
+            map.remove(id).map(|tx| {
+                trace!("{:?} was in log", id);
+                tx
             })
-            .get(&id)
-            .map(|tx| {
-                trace!("cloning sender");
-                tx.clone()
-            })
+        };
+        trace!("release inflight store lock");
+        otx
     }
 }
