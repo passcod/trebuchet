356981de45e1c33decc4437bb0a3e70cc3bd4560
Félix Saparelli <felix@passcod.name>
2019-03-28 14:14:45 +1300

    [rpc] Use hashmap with row locks for inflight log

--- src/inflight.rs
+++ src/inflight.rs
@@ -1,20 +1,16 @@
+use chashmap::CHashMap;
 use crossbeam_channel::{bounded, Receiver, Sender};
 use jsonrpc_core::{Id, Response};
 use log::trace;
-use std::collections::HashMap;
 use std::sync::{
     atomic::{AtomicUsize, Ordering},
-    Arc, Mutex,
+    Arc,
 };
 
 #[derive(Clone, Default)]
 pub struct Inflight {
     counter: Arc<AtomicUsize>,
-
-    // Mutex is appropriate here as we're always doing writes, and never just
-    // reading. So there's no need for any structure that optimises for reads!
-    // Perhaps a hashmap with more granular locking would help, though.
-    store: Arc<Mutex<HashMap<Id, Sender<Response>>>>,
+    store: Arc<CHashMap<Id, Sender<Response>>>,
 }
 
 impl Inflight {
@@ -24,27 +20,18 @@ impl Inflight {
         let (tx, rx) = bounded(0);
 
         {
-            trace!("obtain inflight store lock");
-            let mut map = self.store.lock().expect("inflight store poisoned");
             trace!("insert {:?} into inflight log", id);
-            map.insert(id.clone(), tx);
-            trace!("release inflight store lock");
+            self.store.insert(id.clone(), tx);
         }
 
         (id, rx)
     }
 
     pub fn recall(&self, id: &Id) -> Option<Sender<Response>> {
-        let otx = {
-            trace!("obtain inflight store lock");
-            let mut map = self.store.lock().expect("inflight store poisoned");
-            trace!("remove {:?} from inflight log", id);
-            map.remove(id).map(|tx| {
-                trace!("{:?} was in log", id);
-                tx
-            })
-        };
-        trace!("release inflight store lock");
-        otx
+        trace!("remove {:?} from inflight log", id);
+        self.store.remove(id).map(|tx| {
+            trace!("{:?} was in log", id);
+            tx
+        })
     }
 }
